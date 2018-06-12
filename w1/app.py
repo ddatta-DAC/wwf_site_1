8247af5f05894af6325b71eefc36b7b0b9858f0c
Debanjan Datta <ddatta@vt.edu>
2018-06-12 14:08:35 -0400

    updates

--- w1/app.py
+++ w1/app.py
@@ -19,3 +19,12 @@ def companies():
 @app.route('/trade_by_year')
 def trade_by_year():
     return render_template('trade_by_year.html')
+
+@app.route('/graph_analysis_1')
+def graph_analysis_1():
+    return render_template('graph_analysis_1.html')
+
+
+@app.route('/graph_analysis_2')
+def graph_analysis_2():
+    return render_template('graph_analysis_2.html')
